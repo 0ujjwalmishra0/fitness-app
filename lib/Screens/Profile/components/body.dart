414b9768f87fbde670811a5cdc18e05b0ec0968b
0ujjwalmishra0 <ujjwalcoolguy05@gmail.com>
2020-10-01 15:45:11 +0530

    fixed fetching profile details using SharedPrefs

--- lib/Screens/Profile/components/body.dart
+++ lib/Screens/Profile/components/body.dart
@@ -14,7 +14,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -22,38 +21,19 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  String gmail;
-  String displayName;
-  String photoUrl;
+  String name;
+  String email;
+  String imageUrl;
 
-  Map<String, String> newUser = {};
-  Future _userFuture;
-
+ 
   Future getLocalData() async {
-    final prefs = await SharedPreferences.getInstance();
-    gmail = await prefs.getString('gmail');
-    displayName = await prefs.getString('displayName');
-    photoUrl = await prefs.getString('photoUrl');
-    // print("inside getLocal data");
-    // print("name is $displayName");
-    // print('photo is $photoUrl');
-    // print('gmail is $gmail');
+    SharedPreferences.getInstance().then((value) {
+       name = value.getString('name');
+       email = value.getString('email');
+       imageUrl = value.getString('imageUrl');
+    });
   }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-
-    super.initState();
-    // getLocalData();
-    _userFuture = getUser();
-  }
-
-  getUser() async {
-    final _userData = await DBProvider.db.getUser();
-    return _userData;
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     // final user = Provider.of<signupBody.Body>(context, listen: false);
@@ -65,7 +45,7 @@ class _BodyState extends State<Body> {
       return Padding(
         padding: EdgeInsets.only(
           left: defaultSize * 3,
-          top: defaultSize*1.4,
+          top: defaultSize * 1.4,
         ),
         child: Row(
           children: <Widget>[
@@ -79,7 +59,6 @@ class _BodyState extends State<Body> {
               ),
             ),
             Spacer(),
-            
             Switch(
                 value: themeChange.darkTheme,
                 onChanged: (bool value) {
@@ -88,8 +67,8 @@ class _BodyState extends State<Body> {
                   else
                     themeChange.darkTheme = false;
                 }),
-                SizedBox(
-              width: size.width*0.03,
+            SizedBox(
+              width: size.width * 0.03,
             ),
           ],
         ),
@@ -99,7 +78,7 @@ class _BodyState extends State<Body> {
     return SingleChildScrollView(
       child: FutureBuilder(
         future: getLocalData(),
-        // _userFuture,
+        
         builder: (context, userData) {
           switch (userData.connectionState) {
             case ConnectionState.none:
@@ -108,16 +87,13 @@ class _BodyState extends State<Body> {
               return Container();
             case ConnectionState.active:
             case ConnectionState.done:
-              // if (!newUser.containsKey('displayName')) {
-              //     newUser = Map<String, String>.from(userData.data);
-              //   }
+              
               return Column(
                 children: <Widget>[
                   Info(
-                    image: photoUrl,
-                    // "https://scontent-vie1-1.cdninstagram.com/v/t51.2885-19/101292696_278815523249225_1135093752491147264_n.jpg?_nc_ht=scontent-vie1-1.cdninstagram.com&_nc_ohc=sCh_E0GkJ8cAX_HkBB0&oh=005798e859b99e2c95e60ea9aa8efc8f&oe=5F51237F",
-                    name: displayName,
-                    email: gmail,
+                    image: imageUrl,
+                    name: name,
+                    email: email,
                   ),
                   SizedBox(height: SizeConfig.defaultSize * 2), //20
                   ProfileMenuItem(
